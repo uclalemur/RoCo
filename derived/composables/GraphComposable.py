287b40bed678650f32adee989fe0159021e064d8
Christian Warloe <christian.warloe@gmail.com>
2016-10-25 23:47:23 -0700

    Added docstring to GraphComposable class

---
+++ derived/composables/GraphComposable.py
@@ -0,0 +1,20 @@
+"""The GraphComposable Module
+
+Contains the GraphComposable class, which adds output functionality to the
+graph class
+"""
+
+from fegraph.FaceEdgeGraph import FaceEdgeGraph
+from api.Composable import Composable
+
+class GraphComposable(Composable,FaceEdgeGraph):
+    """Class allowing output to be produced from FaceEdgeGraphs
+
+    The GraphComposable class adds the Composable interface to the
+    FaceEdgeGraph class. With this class, graphs can be converted to drawings
+    and output to a file or drawn using tkinter
+
+    Attributes:
+        component (FoldedComponent): Reference to the FoldedComponent
+            represented by the graph
+    """
