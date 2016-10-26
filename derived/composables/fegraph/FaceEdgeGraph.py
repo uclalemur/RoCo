83655419c4b1f7245cc7cb35fa3576f3c5ae5fda
Christian Warloe <christian.warloe@gmail.com>
2016-10-25 23:02:57 -0700

    Moved files related to Face-Edge Graph into new package fegraph

--- derived/composables/fegraph/FaceEdgeGraph.py
+++ derived/composables/fegraph/FaceEdgeGraph.py
@@ -2,7 +2,7 @@
 Contains the Graph class and several helper functions.
 """
 
-class Graph():
+class FaceEdgeGraph():
     """
     A face-edge graph consisting of Face and HyperEdge objects
     """
---
+++ derived/composables/fegraph/__init__.py
@@ -0,0 +1,5 @@
+"""The Face-Edge graph package
+
+This package contains classes and functions used to construct Face-Edge graphs
+for use in the GraphComposable.
+"""
