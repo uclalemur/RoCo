2a6e45616bdb8f18e83ffb14724431599fff7a17
Prathyush <prathyushk@ucla.edu>
2017-02-14 11:40:45 -0800

    Added composable pydocs

--- roco/api/composable.py
+++ roco/api/composable.py
@@ -12,19 +12,62 @@ class Composable():
     """
 
     def new(self):
+        """Returns a new instance of a Composable.
+
+        Args:
+            None
+
+        Returns:
+            Composable object.
+
+        """
         return self.__class__()
 
     def append(self, new_composable, new_prefix):
+        """Combines two composables with the new one being prefixed.
+
+        Args:
+            new_composable (Composable): the composable that is being appended to this one.
+            new_prefix (str): the prefix of all the names for the composable being appended.
+
+        """
         raise NotImplementedError
 
     def add_component(self, component_obj):
+        """Tells composable about what component it belongs to.
+
+        Args:
+            component_obj (Component): the component to tie this composable to.
+
+        """
         pass
 
     def add_interface(self, new_interface):
+        """Tells composable about an interface
+
+        Args:
+            new_interface: interface to add to composable.
+
+        """
         pass
 
-    def attach(self, from_port, to_port, kwargs):
+    def attach(self, from_port, to_port, **kwargs):
+        """Attaches two ports inside the composable together
+
+        Args:
+            from_port (Port):
+            to_port(Port):
+            kwargs (dict): args relating to the connection between the ports.
+        :return:
+        """
         raise NotImplementedError
 
     def make_output(self, file_dir, **kwargs):
+        """Creates output for the composable.
+
+        Args:
+            file_dir (str): the directory to place to output
+            kwargs (dict): arguments for the output generation
+
+        """
         raise NotImplementedError
