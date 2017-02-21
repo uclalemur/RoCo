5f19d254046ce0bb18a9950811ceb1d668b5a44b
Christian Warloe <cwarloe@ucla.edu>
2017-02-20 18:54:53 -0800

    Added utils module to both derived and api

---
+++ roco/api/utils/__init__.py
@@ -0,0 +1,1 @@
+"""The utils module contains helper classes and functions required by the various api modules"""
---
+++ roco/derived/utils/__init__.py
@@ -0,0 +1,2 @@
+"""The utils module contains helper functions and classes required by the 
+derived modules and their dependent classes"
