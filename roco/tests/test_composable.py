6cbc3e2ac4793191301a7dd7771406d6cae07f3f
M. Doga Dogan <dogan@ucla.edu>
2017-03-06 23:46:55 -0800

    Update test_composable.py

--- roco/tests/test_composable.py
+++ roco/tests/test_composable.py
@@ -2,14 +2,13 @@ import sys
 import os
 import unittest
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from api.composable import Composable  # imports the Composable class
+from api.composable import Composable  # Imports the Composable class
 
 class TestComposable(unittest.TestCase):
 
     def test_new(self):
     	c = Composable()
-        self.assertIsInstance(c, Composable)  # checks if c is an instance of the Composable class
+        self.assertIsInstance(c, Composable)  # Checks if c is an instance of the Composable class
 
 if __name__ == '__main__':
     unittest.main()
-    
