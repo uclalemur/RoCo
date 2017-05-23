dfc880a71e041e509ab0be8c79893f15513899a1
M. Doga Dogan <dogan@ucla.edu>
2017-05-23 00:31:48 -0700

    Added more tests to test_composable.py

--- roco/tests/test_composable.py
+++ roco/tests/test_composable.py
@@ -3,12 +3,22 @@ import os
 import unittest
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from roco.api.composable import Composable  # Imports the Composable class
+from roco.api.interface import Interface  # Imports the Interface class
 
 class TestComposable(unittest.TestCase):
+	def setUp(self):
+        self.c = Composable()
 
     def test_new(self):
-    	c = Composable()
-        self.assertIsInstance(c, Composable)  # Checks if c is an instance of the Composable class
+        self.assertIsInstance(self.c.new, Composable)  # Checks if c.new is an instance of the Composable class
+        
+    def test_add_interface(self):
+    	inter = Interface()
+        try:
+			self.c.add_interface(inter)
+			self.assertEqual(True,True)
+		except:
+			self.assertEqual(True,False)
 
 if __name__ == '__main__':
     unittest.main()
