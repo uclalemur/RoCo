19a4ba4cad286dab6c6914aa77bf376f488a2296
Jiale He <jackhe@g.ucla.edu>
2017-06-01 17:29:47 -0700

    corrected led_pot

--- roco/builders/led_pot.py
+++ roco/builders/led_pot.py
@@ -5,6 +5,6 @@ from roco.library.led import LED
 
 c = Component(name = "LEDPot")
 c.add_subcomponent("pot", "pot")
-c.add_subcomponent("led", "led
+c.add_subcomponent("led", "led")
 c.add_connection(("pot", "vOut"), ("led", "eIn"))
 c.make_output()
