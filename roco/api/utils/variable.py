7b40f7c80a096e839e05b6587507b843f3aaa944
Prathyush <prathyushk@ucla.edu>
2017-11-05 20:22:04 -0800

    Fix variable serialization issue
    For some reason including dummy_index in the serialization and
    deserialization process breaks the fact that two references point
    to the same object once they're deserialized. To workaround this,
    I have removed the dummy_index from the serialization. However,
    this means that a component C is serialized and then deserialized
    into component D, the parameters in C will not be equal to the
    parameters in D because their dummy indices will differ.

--- roco/api/utils/variable.py
+++ roco/api/utils/variable.py
@@ -1,4 +1,4 @@
-from sympy import Dummy
+from sympy import Dummy, Symbol
 
 def eval_equation(equation):
     """Evaluates given sympy expression using the parameters.
@@ -119,14 +119,8 @@ class Variable(Dummy):
         return self.name
 
     def __getstate__(self):
-      state = Dummy.__getstate__(self)
+      state = Symbol.__getstate__(self)
       state['is_solved'] = self.is_solved
       state['default'] = self.default
       state['solved'] = self.solved
       return state
-
-    def __setstate__(self, state):
-      Dummy.__setstate__(self, state)
-      self.is_solved = state['is_solved']
-      self.default = state['default']
-      self.solved = state['solved']
